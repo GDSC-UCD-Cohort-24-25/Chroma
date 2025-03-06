44ec2e84b4e907b7039b812cb976ee7371fcd6e1
Adamy <admye@ucdavis.edu>
2025-03-06 15:24:19 -0800

    frontend vite bug

--- front-end_v1/vite.config.ts
+++ front-end_v1/vite.config.ts
@@ -1,5 +1,5 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -7,4 +7,8 @@ export default defineConfig({
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
-})
+  server: {
+    host: '0.0.0.0',  // Make sure Vite listens on all network interfaces
+    port: process.env.PORT || 3000,  // Use the PORT environment variable or fallback to 3000
+  },
+});
