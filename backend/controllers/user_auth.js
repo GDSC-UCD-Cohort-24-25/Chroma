388b502179ec1408f2939c12a159a7ed0f211737
Adamy <admye@ucdavis.edu>
2025-03-05 16:00:27 -0800

    jwt access&refreshtoken

--- backend/controllers/user_auth.js
+++ backend/controllers/user_auth.js
@@ -1,5 +1,8 @@
+import dotenv from 'dotenv';
+dotenv.config();
 import User from '../models/User.js';
 import bcrypt from 'bcryptjs';
+import jwt from 'jsonwebtoken';
 
 export const register = async (req, res) => {
     try {
@@ -17,17 +20,35 @@ export const login = async (req, res) => {
     try {
         const { email, password } = req.body;       //password here neeed 2b hashed
         const user = await User.findOne({ email: email });
+        // Authenticate
         if (!user) {
             return res.status(400).json({ success:false, message: "User not found with this email" });
         }
         if (!(await bcrypt.compare( password, user.password ))) {
             return res.status(400).json({ success:false, message: "Invalid password" });
         }
+        // Create JWT token
+        const payload = {
+            user: {
+                id: user._id
+            }
+        }
+        const accessToken = generateAccessToken(payload);   // short lived
+        const refreshToken = jwt.sign(user, process.env.REFRESH_TOKEN_SECRET, { expiresIn: '7d'});   // long lived
+        // cookie1:2h     maxAge = 24 hours > token expiry time. This enable generate "your login expired" message.
+        res.cookie('accessToken', accessToken, { httpOnly: true, secure: true, sameSite: 'none', maxAge: 24*60*60*1000 }); 
+        // cookie2:7d
+        res.cookie('refreshToken', refreshToken, { httpOnly: true, secure: true, sameSite: 'none', maxAge: 7*24*60*60*1000 });
+
         res.status(200).json({ success:true, message:"Logged in successfully", email: user.email});
     }
     catch (error) {
         res.status(400).json({ success:false, message: error.message });
     }
+}
+
+function generateAccessToken(payload) {
+    return jwt.sign(payload, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '2h' });
 }
 
 // export const deleteUser = async (req, res) => {
