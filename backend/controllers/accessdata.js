213ecf484b3a9b6b44aa7b0bf01641f30e8bcc98
Adamy <admye@ucdavis.edu>
2025-03-06 02:07:18 -0800

    auth middlewaregit add .! get

---
+++ backend/controllers/accessdata.js
@@ -0,0 +1,33 @@
+// do authmiddleware & REST on db
+
+import Budget from "../models/budget";
+import jwt from "jsonwebtoken";
+
+export const auth = (req, res, next) => {   // middleware
+  try {
+    // const accessToken = req.headers.authorization.split(" ")[1];
+    const accessToken = req.cookies.accessToken;
+    if (!accessToken) {
+        return res.status(401).json({ success: false, message: "Access token missing" });
+    }
+    // jwt vertify with callback!!
+    jwt.verify(accessToken, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+        if (err) {
+            return res.status(403).json({ success: false, message: "Invalid or expired token" });
+        }
+        req.user = decoded.user; // Attach the decoded user to the request object
+        next();
+    });
+  } catch (error) {
+    res.status(401).json({ success: false, message: "Not authorized to access this route" });
+  }
+}
+
+export const getBudgets = async (req, res) => {
+  try {
+    const budgets = await Budget.find();
+    res.status(200).json({ success: true, data: budgets });
+  } catch (error) {
+    res.status(400).json({ success: false, message: error.message });
+  }
+}
