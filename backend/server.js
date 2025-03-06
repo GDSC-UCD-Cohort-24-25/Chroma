18037ffb1220969d6392a884b0fc78a7c2eed1e4
Adamy <admye@ucdavis.edu>
2025-03-06 04:24:45 -0800

    cros

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,22 @@
 import dotenv from 'dotenv';
 dotenv.config();
 import express from 'express';
+import cookieParser from 'cookie-parser';
+import cors from 'cors';
 import { connectDB } from './config/db.js';
 import userRoutes from './routes/router.js';
 
 const PORT = process.env.NodeJS_PORT || 3000;
 
+const corsOptions = {
+    origin: 'http://localhost:3000',  // Your client URL
+    credentials: true,  // Allow cookies to be sent
+};
+
 const app = express();
+app.use(cors(corsOptions));
 app.use(express.json());
+app.use(cookieParser());// enable cookie parser
 app.use("/", userRoutes);       //  /auth  &  /budgets
 
 
