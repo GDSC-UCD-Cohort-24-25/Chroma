f71ff23991fa66443ca6c7da522e585d6fb474ab
usrnme20 <bhavyapaleru@gmail.com>
2025-04-18 17:11:00 -0700

    help

--- front-end_v1/src/SetupPage.tsx
+++ front-end_v1/src/SetupPage.tsx
@@ -21,9 +21,10 @@ const Setup = () => {
   };
 
   return (
-    <div className="min-h-screen flex items-center justify-center bg-gradient-to-br from-pink-100 via-purple-100 to-rose-100 p-6">
-      <div className="max-w-md w-full bg-white/90 p-8 rounded-3xl shadow-xl backdrop-blur-md">
-        <h2 className="text-3xl font-bold text-transparent bg-clip-text bg-gradient-to-r from-pink-500 to-purple-500 mb-6 text-center">
+    <div className="min-h-screen flex items-center justify-center bg-[#F4F4EA]">
+
+      <div className="max-w-md w-full bg-white p-8 rounded-3xl shadow-xl backdrop-blur-md">
+        <h2 className="text-3xl font-bold text-transparent bg-clip-text p-4 bg-[#92BAA4] text-center">
           Welcome to CowCulator!
         </h2>
         <p className="text-lg text-gray-700 text-center mb-6">
