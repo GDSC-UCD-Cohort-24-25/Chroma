8674cc5082a5d06bfc66f519eb1801b94c91b722
Camila Niño <camila.francia2004@gmail.com>
2025-05-03 23:16:37 -0700

    Updated layout with navBar

--- front-end_v1/src/layouts/layout.tsx
+++ front-end_v1/src/layouts/layout.tsx
@@ -1,16 +1,58 @@
-import React from 'react';
-import Header from '../header';
-
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import { GiHamburgerMenu } from 'react-icons/gi';
+import { GrClose } from 'react-icons/gr';
+import { MdHome, MdDashboard } from 'react-icons/md';
 import { ReactNode } from 'react';
+import { useLocation } from 'react-router-dom';
+import { useAuth } from '../AuthContext'; // Adjust the import path as necessary
+
 
 function Layout({ children }: { children: ReactNode }) {
- return (
-    <div className="min-h-screen flex flex-col">
-      <Header />
-      <main className="flex-grow">{children}</main> 
+  const [open, setOpen] = useState(true);
+  const location = useLocation();
+  //const { isAuthenticated } = useAuth();
+
+  const showDashboardLink = location.pathname ==='/budget';  //isAuthenticated && location.pathname === '/budget';
+
+  return (
+    <div className="flex min-h-screen">
+      {/* Sidebar */}
+      <aside className={`transition-all duration-300 ${open ? 'w-52' : 'w-16'} flex flex-col px-3 py-5`}
+        style={{ backgroundColor: '#7DAE94' }}>
+        <div className="flex items-center justify-between">
+          {open && 
+          <span className="text-2xl font-bold text-white">
+            CowCulator
+          </span>}
+          <button onClick={() => setOpen(!open)} className={`text-white ${open ? '' : 'items-center mx-auto'}`}>
+            {open ? <GrClose size={22} /> : <GiHamburgerMenu size={26} />}
+          </button>
+        </div>
+
+        <nav className={`mt-10 space-y-4 ${open ? '' : 'items-center mx-auto'}`}>
+          <Link to="/" className="flex items-center gap-3 text-white hover:text-white/90 text-lg">
+            <MdHome size={30} />
+            {open && <span className="text-lg">Home</span>}
+          </Link>
+          
+          {showDashboardLink && (
+            <Link
+              to="/budget"
+              className="flex items-center gap-3 text-white hover:text-white/90 text-lg"
+            >
+              <MdDashboard size={30} />
+              {open && <span>Dashboard</span>}
+            </Link>
+          )}
+
+        </nav>
+      </aside>
+
+      {/* Main Content */}
+      <main className="flex-1 overflow-auto">{children}</main>
     </div>
   );
-
 }
 
 export default Layout;
