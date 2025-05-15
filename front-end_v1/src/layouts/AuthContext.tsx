e2262130b79942822731552d280fe99f282cae34
Camila Niño <camila.francia2004@gmail.com>
2025-05-15 02:17:17 -0700

    updated refresh and checkauth functions

--- front-end_v1/src/layouts/AuthContext.tsx
+++ front-end_v1/src/layouts/AuthContext.tsx
@@ -1,5 +1,6 @@
 import React, { createContext, useContext, useState, useEffect } from 'react';
 const API_BASE_URL = import.meta.env.VITE_API_BASE_URL|| 'http://localhost:3000';
+console.log('VITE_BASE_URL:', API_BASE_URL);
 import {refreshPage} from '../services/apiService'
 
 type AuthContextType = {
@@ -14,7 +15,6 @@ type AuthContextType = {
   getName: () => string | null;
   getTotal: () => number;
   getEmail: () => string | null;
-  refreshUserInfo: () => Promise<void>;
 };
 
 const AuthContext = createContext<AuthContextType | undefined>(undefined);
@@ -25,7 +25,9 @@ export const AuthProvider = ({ children }: { children: React.ReactNode }) => {
   const [name, setName] = useState<string | null>(null);
   const [email, setEmail] = useState<string | null>(null);
   const [total, setTotal] = useState(0);
-  const refreshUserInfo = async () => {
+  
+  const checkAuth = async () => {
+    console.log('cheking authentication fetch');
     try {
       const res = await fetch(`${API_BASE_URL}/api/checkstatus`, {
         method: 'GET',
@@ -33,47 +35,7 @@ export const AuthProvider = ({ children }: { children: React.ReactNode }) => {
         credentials: 'include',
       });
       const data = await res.json();
-      if (data.success) {
-        setName(data.name);
-        setEmail(data.email);
-        setTotal(data.total);
-      }
-      else{throw Error('Could not be authenticated');}
-    } catch (err:any) {
-      console.log(err.message || "Failed to refresh user info");
-    }
-  };
-  
-  const refreshAuth = async () => {
-    try {
-      const res = await refreshPage();
-      if (res.success) {
-        setIsAuthenticated(true);
-        console.log('Token refreshed');
-      } else {
-        setIsAuthenticated(false);
-        console.log('Refresh failed');
-      }
-    } catch (err: any) {
-      setIsAuthenticated(false);
-      console.log("Refresh failed:", err);
-    }
-  };
-  
-  useEffect(() => {
-    const checkAuth = async () => {
-      try {
-        console.log('cheking authentication fetch');
-        const res = await fetch(`${API_BASE_URL}/api/checkstatus`, { 
-          method: 'GET',
-          headers: {
-            'Content-Type': 'application/json',
-          },
-          credentials: 'include',
-        });
-        console.log(res);
-        const data = await res.json();
-        if (!data.success) {
+      if (!data.success) {
           throw Error('Could not be authenticated');
         }
         setIsAuthenticated(true);
@@ -88,12 +50,39 @@ export const AuthProvider = ({ children }: { children: React.ReactNode }) => {
         console.log('loading');
         setLoading(false);
       }
-    };
-
-    checkAuth();
+  };
+  
+  const refreshAuth = async () => {
+    try {
+      const res = await refreshPage();
+      if (!res.success) {
+        throw Error('Could not refresh token');
+      }
+      if (res.success) {
+        setIsAuthenticated(true);
+        console.log('Token refreshed');
+      } else {
+        setIsAuthenticated(false);
+        console.log('Refresh failed');
+      }
+    } catch (err: any) {
+      setIsAuthenticated(false);
+      console.log("Refresh failed:", err);
+      
     
+    }
+  }
+  
+  useEffect(() => {
+    checkAuth();
 
   }, []);
+
+  useEffect(() => {
+    if(isAuthenticated && name==null) checkAuth();
+    
+
+  }, [isAuthenticated]);
 
   const login = () => setIsAuthenticated(true);
   const logout = () => {
@@ -106,7 +95,7 @@ export const AuthProvider = ({ children }: { children: React.ReactNode }) => {
   const getEmail = () => email;
   
   return (
-    <AuthContext.Provider value={{ isAuthenticated, loading, name, total, email, login, logout, refreshAuth, getName, getTotal, getEmail, refreshUserInfo}}>
+    <AuthContext.Provider value={{ isAuthenticated, loading, name, total, email, login, logout, refreshAuth, getName, getTotal, getEmail}}>
       {children}
     </AuthContext.Provider>
   );
