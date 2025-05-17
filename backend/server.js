25dd60148140a90dd3db18837d98b64d26ec6f69
Aishwari Sirur <137564086+acarva203@users.noreply.github.com>
2025-05-16 21:42:06 -0700

    Update server.js with old code

--- backend/server.js
+++ backend/server.js
@@ -7,28 +7,13 @@ import { connectDB } from './config/db.js';
 import userRoutes from './routes/router.js';
 
 const PORT = process.env.NodeJS_PORT || 3000;
-const allowedOrigins = ['https://aggiepantry.org', 'http://aggiepantry.org'];
+
 const corsOptions = {
-    origin: function (origin, callback) {
-        // allow requests with no origin
-        // (like mobile apps or curl requests)
-        if (!origin) return callback(null, true);
-        if (allowedOrigins.includes(origin)) {
-            return callback(null, true);
-        }else {
-            return callback(new Error('Not allowed by CORS'));
-        }
-    },
+    origin: process.env.FRONTEND_URL || 'http://localhost:3000',  // Replace with your frontend URL
     credentials: true,  // Allow cookies to be sent
 };
 
 const app = express();
-app.options('*', (req, res) => {
-  res.setHeader('Access-Control-Allow-Origin', 'https://aggiepantry.org');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-  res.sendStatus(200);
-});
 app.use(cors(corsOptions));
 app.use(express.json());
 app.use(cookieParser());// enable cookie parser
